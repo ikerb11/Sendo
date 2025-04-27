eeb0ee1d3691dfaa501b7da195286665e2f0ee8b
ikerb11 <ikerberna0@gmail.com>
2025-04-27 17:41:51 +0200

    Matquetacion y CSS Basico0 perfil

--- sendo/src/app/perfil/perfil.component.ts
+++ sendo/src/app/perfil/perfil.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { TarjetasVinculadasComponent } from '../tarjetas-vinculadas/tarjetas-vinculadas.component';
 
 @Component({
   selector: 'app-perfil',
-  imports: [],
+  imports: [TarjetasVinculadasComponent],
   template: `<h1>Perfil</h1>`,
   templateUrl: './perfil.component.html',
   styleUrl: './perfil.component.css'
